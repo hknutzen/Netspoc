51e45240d87a1b7fe98abd1d9a27cbdf7c57e427
Heinz Knutzen <heinz.knutzen@dataport.de>
2002-02-14 14:44:26 +0000

    - Changed locking - other chmod

--- cvs-support/newpolicy.pl
+++ cvs-support/newpolicy.pl
@@ -16,6 +16,7 @@
 
 use strict;
 use warnings;
+use Fcntl qw/:flock/; # import LOCK_* constants
 use constant CURRENT => 'current';
 use constant NETSPOC => 'netspoc';
 
@@ -27,11 +28,13 @@ my $code = 'code.acl';
 my $policydb = "$project/" . NETSPOC;
 # link to current policy
 my $link = "$policydb/" . CURRENT;
+my $lock = "$policydb/LOCK";
 my $home = $ENV{HOME};
 # users working directory
 my $working = "$home/" . NETSPOC;
 
-my $pdir = readlink "$link" or die "Can't read $link\nAnother $0 running?\n";
+my $pdir = readlink "$link" or
+    die "Can't read link " . CURRENT . "in $policydb. Check and repair manually.\n";
 # strip trailing slash
 $pdir =~ s'/$'';
 # link may be relative or absolute
@@ -41,15 +44,23 @@ my($policy) = ($pdir =~ m'([^/]+$)') or die "Error: can't extract basename from 
 my($count) = ($policy =~ /^p(\d+)$/) or die "Error: invalid policy name: $policy";
 
 # user must have checked in current policy
-chdir $working or die "Error: can't change do $working: $!\n";
+chdir $working or die "Error: can't change to $working: $!\n";
 system("cvs -nQ tag -c test") == 0 or die "Aborted\n";
 
-# Semaphore, lock policy database
-unlink $link or die "Error: can't remove $link\nAnother $0 running?\n";
+# Lock policy database
+open LOCK, ">$lock" or die "Error: couldn't open $lock: $!";
+flock(LOCK, LOCK_EX | LOCK_NB) or die "Abort: Another $0 is running\n";
 
 # increment policy counter
 $count++;
 $policy = "p$count";
+$pdir = "$policydb/$policy";
+if(-d $pdir) {
+    warn "Skipping unfinished policy $policy\n";
+    $count++;
+    $policy = "p$count";
+    $pdir = "$policydb/$policy";
+}
 print STDERR "Saving policy $count\n";
 
 # tagging policy
@@ -57,8 +68,7 @@ system("cvs -Q tag -c $policy") == 0 or die "Aborted\n";
 
 # check out new policy into policy database
 chdir $policydb or die "Error: can't cd to $policydb: $!\n";
-$pdir = "$policydb/$policy";
-mkdir $policy or die "Error: can't create $pdir: $!\n";
+mkdir $pdir or die "Error: can't create $pdir: $!\n";
 chdir $policy or die "Error: can't cd to $pdir: $!\n";
 system("cvs -Q checkout -d src -r $policy " . NETSPOC) == 0 or
     die "Error: can't checkout $policy to $pdir/src\n";
@@ -68,11 +78,13 @@ chdir $pdir or die "Error: can't cd to $pdir: $!\n";
 print STDERR "Compiling policy $count\n";
 system("$compiler src > $code") == 0 or warn "$compiler failed: $?\n";
 # make new policy read only
-system("chmod -R a-w .") == 0 or warn "Can't make $pdir read only\n";
+system("chmod -R a-w *") == 0 or warn "Can't make $pdir read only\n";
 
-# Semaphore, unlock policy database
 chdir $policydb or die "Error: can't cd to $policydb: $!\n";
+unlink $link or die "Error: can't remove $link: $!\n";
 symlink $policy, CURRENT or
     die "Error: failed to create symlink '" . CURRENT . "' to $policy in $policydb\n";
 
 print STDERR "Updating policy $count complete\n";
+
+# Unlock policy database: implicitly by exit
