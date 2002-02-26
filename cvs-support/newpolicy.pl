8e113259f609820cd6447232ebdee7fea146a386
Heinz Knutzen <heinz.knutzen@gmx.de>
2002-02-26 10:51:45 +0000

    - $HOME/netspoc/POLICY - check compiler status - GPL

--- cvs-support/newpolicy.pl
+++ cvs-support/newpolicy.pl
@@ -1,68 +1,91 @@
 #!/usr/bin/perl
-# File: update
-# Author: Heinz Knutzen
-# Address: heinz.knutzen@web.de, heinz.knutzen@dzsh.de
+# newpolicy -- integrates NetSPoC with CVS
+# http://netspoc.berlios.de
+# (c) 2002 by Heinz Knutzen <heinz.knutzen@web.de>,<heinz.knutzen@dzsh.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 # Description:
-# Integrates netspoc with version control / build management
-# Should be called from a user in a CVS controled directory with
-# netspoc configuration files.
+# Integrates NetSPoC with version control / build management
+# The current user must have a working directory 'netspoc'
+# in his home directory with NetSPoC files checked out
+# from the CVS repository.
+# - checks if working directory 
+#   - is updated and 
+#   - all changes are commited, 
+#   - aborts if not
 # - identifies the current policy 
-# - checks if all changes are commited, aborts if not
 # - calculates the next policy tag
 # - tags users current configuration with new policy tag
 # - extracts the tagged configuration into the policy database
 # - compiles the new policy
 # - marks the new directory as current policy
+#
+# $Id$
 
 use strict;
 use warnings;
-use Fcntl qw/:flock/; # import LOCK_* constants
-use constant CURRENT => 'current';
-use constant NETSPOC => 'netspoc';
+use Fcntl qw(:DEFAULT :flock);
 
-my $id = '$Id$ ';
-my $project = '/home/madnes';
-my $compiler = 'netspoc.pl';
-
+my $project = '/home/hk/project';
+my $compiler = '/home/hk/develop/netspoc';
+my $log = 'compile.log';
+# name of cvs module
+my $module = 'netspoc';
 # policy database
-my $policydb = "$project/" . NETSPOC;
+my $policydb = "$project/netspoc";
 # link to current policy
-my $link = "$policydb/" . CURRENT;
+my $link = "$policydb/current";
+# LOCK for preventing concurrent updates
 my $lock = "$policydb/LOCK";
 my $home = $ENV{HOME};
 # users working directory
-my $working = "$home/" . NETSPOC;
+my $working = "$home/netspoc";
+# policy file, contains current policy number
+my $pfile = "$working/POLICY";
+$ENV{CVSROOT} or die "Abort: No CVSROOT specified!\n";
 
-$ENV{CVSROOT} or die "Abort:  No CVSROOT specified!\n";
-
-my $pdir = readlink "$link" or
-    die "Can't read link " . CURRENT . "in $policydb. Check and repair manually.\n";
-# strip trailing slash
-$pdir =~ s'/$'';
-# link may be relative or absolute
-$pdir = "$policydb/$pdir" unless $pdir =~ m'^/';
--d $pdir or die "$pdir isn't a directory\n";
-my($policy) = ($pdir =~ m'([^/]+$)') or die "Error: can't extract basename from $pdir\n";
-my($count) = ($policy =~ /^p(\d+)$/) or die "Error: invalid policy name: $policy";
-
-# user must have checked in current policy
+# user must have an updated and checked in current policy
 chdir $working or die "Error: can't change to $working: $!\n";
 system("cvs -nQ tag -c test") == 0 or die "Aborted\n";
-
+    
 # Lock policy database
-open LOCK, "$lock" or die "Error: couldn't open $lock: $!";
+sysopen LOCK, "$lock", O_RDONLY | O_CREAT or
+    die "Error: can't open $lock: $!";
 flock(LOCK, LOCK_EX | LOCK_NB) or die "Abort: Another $0 is running\n";
 
+# update, read, increment,commit current policy from working directory
+# update
+system("cvs update $pfile") == 0 or die "Aborted\n";
+# read
+open PFILE, $pfile or die "Can't open $pfile: $!\n";
+my $line = <PFILE>;
+close PFILE;
+# $pfile contains one line: "! p22 comment .."
+my(undef, $policy) = split ' ', $line;
+my($count) = ($policy =~ /^p(\d+)$/) or
+    die "Error: found invalid policy name '$policy' in $pfile";
 # increment policy counter
 $count++;
 $policy = "p$count";
-$pdir = "$policydb/$policy";
-if(-d $pdir) {
-    warn "Skipping unfinished policy $policy\n";
-    $count++;
-    $policy = "p$count";
-    $pdir = "$policydb/$policy";
-}
+open PFILE, ">", $pfile or die "Can't open $pfile: $!\n";
+print PFILE "# $policy # current policy, don't edit manually!\n";
+close PFILE;
+# commit
+system("cvs commit $pfile") == 0 or die "Aborted\n";
+
 print STDERR "Saving policy $count\n";
 
 # tagging policy
@@ -70,24 +93,42 @@ system("cvs -Q tag -c $policy") == 0 or die "Aborted\n";
 
 # check out new policy into policy database
 chdir $policydb or die "Error: can't cd to $policydb: $!\n";
+my $pdir = "$policydb/$policy";
 mkdir $pdir or die "Error: can't create $pdir: $!\n";
 chdir $policy or die "Error: can't cd to $pdir: $!\n";
-system("cvs -Q checkout -d src -r $policy " . NETSPOC) == 0 or
+system("cvs -Q checkout -d src -r $policy $module") == 0 or
     die "Error: can't checkout $policy to $pdir/src\n";
 
 # compile new policy
 chdir $pdir or die "Error: can't cd to $pdir: $!\n";
 print STDERR "Compiling policy $count\n";
-system("$compiler src code ") == 0 or warn "$compiler failed: $?\n";
+my $failed;
+open COMPILE, '-|', "$compiler src code" or
+    die "Can't execute $compiler: $!\n";
+open LOG, '>', $log or die "Can' open $log: $!\n";
+while(<COMPILE>) {
+    print LOG; 
+    print STDERR;
+}
+close LOG;
+close COMPILE;
+($? == 0) or $failed = 1;
+
 # make new policy read only
 system("chmod -R a-w *") == 0 or warn "Can't make $pdir/* read only\n";
 system("chmod a+w .") == 0 or warn "Can't make $pdir world writable\n";
 
-chdir $policydb or die "Error: can't cd to $policydb: $!\n";
-unlink $link or die "Error: can't remove $link: $!\n";
-symlink $policy, CURRENT or
-    die "Error: failed to create symlink '" . CURRENT . "' to $policy in $policydb\n";
-
-print STDERR "Updating policy $count complete\n";
-
+if($failed) {
+    print STDERR "New policy failed to compile\n";
+    my $current = readlink $link;
+    $current and print STDERR "Left current policy as '$current'\n";
+} else {
+    # mark new policy as current, only if compiled successfully
+    chdir $policydb or die "Error: can't cd to $policydb: $!\n";
+    unlink $link;
+    symlink $policy, $link or
+	die "Error: failed to create symlink $link to $policy\n";
+    
+    print STDERR "Updated current policy to '$policy'\n";
+}
 # Unlock policy database: implicitly by exit
