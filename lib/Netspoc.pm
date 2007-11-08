480fe628d1aa60b95c366f18087f504e0d216e9f
Heinz Knutzen <heinz.knutzen@dataport.de>
2007-11-08 13:39:09 +0000

    Added minimal POD

--- lib/Netspoc.pm
+++ lib/Netspoc.pm
@@ -1,24 +1,27 @@
 #!/usr/bin/perl
-# Netspoc.pm
-# A Network Security Policy Compiler
-# http://netspoc.berlios.de
-# (c) 2005 by Heinz Knutzen <heinzknutzen@users.berlios.de>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# $Id$
+
+=head1 Netspoc, a Network Security Policy Compiler
+
+http://netspoc.berlios.de
+(c) 2007 by Heinz Knutzen <heinzknutzen@users.berlios.de>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+$Id$
+
+=cut
 
 package Netspoc;
 use strict;
