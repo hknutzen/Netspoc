9216c69f97e0db1ae0ee639afd0e7bc609955418
Heinz Knutzen <heinz.knutzen@dataport.de>
2011-07-05 15:12:57 +0000

    Spelling

--- lib/Netspoc.pm
+++ lib/Netspoc.pm
@@ -10375,7 +10375,7 @@ sub check_and_convert_routes () {
 		if(@$hops != @{ $hop1->{redundancy_interfaces}}) {
 		    my ($network) = grep { $_ eq $net_ref } 
 		                      values %{ $interface->{routes}->{$hop1} };
-		    # Test for loopback isn't realy needed.
+		    # Test for loopback isn't really needed.
 		    # It's only a temporary hack to prevent some warnings.
 		    $network->{loopback} or
 		    warn_msg "$network->{name} is reached via $hop1->{name}",
