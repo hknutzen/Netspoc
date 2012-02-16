9ef3da15a407784be17bb4de386f5e74d8e1f0f5
Heinz Knutzen <heinz.knutzen@dataport.de>
2012-02-16 15:44:58 +0100

    Fixed severe typo in commit d5f237b2

--- lib/Netspoc.pm
+++ lib/Netspoc.pm
@@ -4622,7 +4622,7 @@ sub convert_hosts() {
                     # Search for enclosing subnet.
                     for (my $j = $i + 1 ; $j < @inv_prefix_aref ; $j++) {
                         my $mask = prefix2mask(32 - $j);
-                        $ip = $i & $mask; # Perl bug #108480
+                        $ip = $ip & $mask; # Perl bug #108480
                         if (my $up = $inv_prefix_aref[$j]->{$ip}) {
                             $subnet->{up} = $up;
                             last;
