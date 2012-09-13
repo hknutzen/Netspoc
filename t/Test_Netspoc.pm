4c4d60e7dc6a8bbfb7ef07cf6959698edb916adb
Heinz Knutzen <heinz.knutzen@dataport.de>
2012-09-13 12:19:44 +0200

    Temp file no longer needed, read from stdin.

--- t/Test_Netspoc.pm
+++ t/Test_Netspoc.pm
@@ -3,26 +3,21 @@ package Test_Netspoc;
 
 use strict;
 use warnings;
-use IPC::Run3;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(compile compile_err get_block);
 
 use Test::More;
-use File::Temp qw/ tempfile tempdir /;
+use IPC::Run3;
 
 my $options = '-quiet -check_redundant_rules=0 -check_service_unknown_owner=0';
 
 sub run {
     my($input) = @_;
 
-    my ($fh, $filename) = tempfile(UNLINK => 1);
-    print $fh $input;
-    close $fh;
-
-    my $cmd = "perl -I lib bin/netspoc $options $filename";
+    my $cmd = "perl -I lib bin/netspoc $options -";
     my ($stdout, $stderr);
-    run3($cmd, \undef, \$stdout, \$stderr);
+    run3($cmd, \$input, \$stdout, \$stderr);
     my $status = $?;
     return($status, $stdout, $stderr);
 }
