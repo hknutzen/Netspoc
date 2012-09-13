9da642e6719b57f9dca2fc6746f7978b2d1546ca
Heinz Knutzen <heinz.knutzen@dataport.de>
2012-09-13 12:07:39 +0200

    New sub compile_err allows to check stderr.
    switched from open to IPC::Run3 to capture stdout and stderr

--- t/Test_Netspoc.pm
+++ t/Test_Netspoc.pm
@@ -3,36 +3,48 @@ package Test_Netspoc;
 
 use strict;
 use warnings;
+use IPC::Run3;
 
 our @ISA    = qw(Exporter);
-our @EXPORT = qw(compile get_block);
+our @EXPORT = qw(compile compile_err get_block);
 
 use Test::More;
 use File::Temp qw/ tempfile tempdir /;
 
 my $options = '-quiet -check_redundant_rules=0 -check_service_unknown_owner=0';
-sub compile {
-    my($config) = @_;
+
+sub run {
+    my($input) = @_;
 
     my ($fh, $filename) = tempfile(UNLINK => 1);
-    print $fh $config;
+    print $fh $input;
     close $fh;
 
     my $cmd = "perl -I lib bin/netspoc $options $filename";
-    open(COMPILE, '-|', $cmd) or die "Can't execute $cmd: $!\n";
+    my ($stdout, $stderr);
+    run3($cmd, \undef, \$stdout, \$stderr);
+    my $status = $?;
+    return($status, $stdout, $stderr);
+}
 
-    # Undef input record separator to read all output at once.
-    $/ = undef;
-    my $output = <COMPILE>;
-    if (not close(COMPILE)) {
-	$! and  die "Syserr closing pipe from $cmd: $!\n";
-	my $exit_value = $? >> 8;
+sub compile {
+    my($input) = @_;
+    my ($status, $stdout, $stderr) = run($input);
 
-	# 0: Success, 1: compare found diffs
- 	$exit_value == 0 || $exit_value == 1 or 
-	    die "Status from $cmd: $exit_value\n";
+    # 0: Success, 1: compare found diffs
+    $status == 0 || $status == 1 or 
+	    die "Status from compile $status\n";
+    $stderr and die "Unexpected output on STDERR:\n$stderr\n";
+    return($stdout);
+}
+
+sub compile_err {
+    my($input) = @_;
+    my ($status, $stdout, $stderr) = run($input);
+    if ($stderr) {
+        $stderr =~ s/\nAborted with \d+ error\(s\)$//ms;
     }
-    return($output);
+    return($stderr);
 }
 
 # Find lines in $data which equal elements in @find.
