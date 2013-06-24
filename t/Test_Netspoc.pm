f827d0ae916ebe88b03964b5b9f7909db4d87d4f
Heinz Knutzen <heinz.knutzen@dataport.de>
2013-06-24 15:06:18 +0200

    Build results of 135b98f (on master)

--- t/Test_Netspoc.pm
+++ t/Test_Netspoc.pm
@@ -25,8 +25,8 @@ sub run {
 sub compile {
     my($input) = @_;
     my ($status, $stdout, $stderr) = run($input);
-    $status == 0 or die "Unexpected status: $status\n";
-    $stderr and die "Unexpected output on STDERR:\n$stderr\n";
+    $status == 0 or return("Failed:\n$stderr\n");
+    $stderr and return("Unexpected output on STDERR:\n$stderr\n");
     return($stdout);
 }
 
