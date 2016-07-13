d1d21f066ba36d2055ab7ee791ab2e80ab8f9a2d
Heinz Knutzen <heinz.knutzen@dataport.de>
2016-07-13 17:55:44 +0200

    Removed workaround for old Perl bug #108480

--- lib/Netspoc/Compiler/Pass1.pm
+++ lib/Netspoc/Compiler/Pass1.pm
@@ -5204,7 +5204,7 @@ sub convert_hosts {
                 # Search for enclosing subnet.
                 for (my $j = $i + 1 ; $j < @inv_prefix_aref ; $j++) {
                     my $mask = prefix2mask(32 - $j);
-                    $ip = $ip & $mask;    # Perl bug #108480
+                    $ip &= $mask;
                     if (my $up = $inv_prefix_aref[$j]->{$ip}) {
                         $subnet->{up} = $up;
                         check_host_compatibility($subnet, $up);
