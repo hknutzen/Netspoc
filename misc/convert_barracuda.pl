3449e5644dc54a3c8f39ee926a28902ef3db255f
Heinz Knutzen <heinz.knutzen@dataport.de>
2016-12-20 11:42:14 +0100

    Indentation; sort ports.

--- misc/convert_barracuda.pl
+++ misc/convert_barracuda.pl
@@ -64,7 +64,10 @@ sub cleanup_prt {
         $prt =~ s/  / /;
         $result{$prt} = 1;
     }
-    return [ sort keys %result ];
+    return [ map { $_->[0] }
+             sort { $a->[1] <=> $b->[1] || $a->[0] cmp $b->[0] }
+             map { [ $_, /(?:tcp|udp) (\d+)/ ? $1 : 0 ] }
+             keys %result ];
 }
 
 sub normalize {
@@ -96,21 +99,55 @@ sub new_name {
     $ip =~ s/[.]/_/g;
     my $name;
     if (my ($ip1, $len) = $ip =~ /(.*)\/(.*)/) {
-        $name = "network:$ip1-$len";
+        $name = "network:INTERNET_$ip1-$len";
     }
     else {
-        $name = "host:$ip";
+        $name = "host:INTERNET_$ip";
     }
     $new_name2ip{$name} = $orig_ip;
     $ip2name{$orig_ip} = $name;
     return $name;
 }
 
+sub quad2int {
+    ($_[0] =~ /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/) or return;
+    ($1 < 256 && $2 < 256 && $3 < 256 && $4 < 256) or return;
+    return $1 << 24 | $2 << 16 | $3 << 8 | $4;
+}
+
 sub write_new_objects {
-    for my $name (sort keys %new_name2ip) {
-        my $ip = $new_name2ip{$name};
-        say "$name = { ip = $ip; }";
-    }    
+    if (my @hosts =
+        map { $_->[0] }
+        sort { $a->[1] <=> $b->[1] }
+        map { [ $_, quad2int($new_name2ip{$_}) ] }
+        grep { /host:/ } keys %new_name2ip)
+    {
+        my $max_name = 0;
+        my $max_ip = 0;
+        for my $name (@hosts) {
+            if (length($name) > $max_name) {
+                $max_name = length($name);
+            }
+            my $ip = $new_name2ip{$name};
+            if (length($ip) > $max_ip) {
+                $max_ip = length($ip);
+            }
+        }
+        for my $name (@hosts) {
+            my $name_space = $max_name - length($name) + 1;
+            my $tabs = int($name_space / 8) + 1;
+            my $ip = $new_name2ip{$name};
+            my $ip_space = $max_ip - length($ip) + 1;
+            say $name, "\t" x $tabs, "= { ip = $ip;", " " x $ip_space ,"}";
+        }
+        say '';
+    }
+    if (my @networks = sort grep { /network:/ } keys %new_name2ip) {
+        for my $name (@networks) {
+            my $ip = $new_name2ip{$name};
+            say "$name = { ip = $ip; }";
+        }
+    }
 }
 
 sub write_list {
@@ -126,13 +163,22 @@ sub write_list {
 
 sub write_service {
     my ($name, $action, $src_list, $dst_list, $prt_list) = @_;
+
+    # Name in Großbuchstaben und mit Unterstrichen.
+    $name = uc $name;
+    $name =~ s/-/_/g;
     say "service:$name = {";
+    say '';
+    say " description = ;";
+    say '';
     print " user =";
     write_list(1, $src_list);
+    say '';
     say " $action\tsrc =\tuser;";
     print "\tdst =";
     write_list(2, $dst_list);
     print "\tprt =";
     write_list(2,$prt_list);
     say "}";
+    say '';
 }
