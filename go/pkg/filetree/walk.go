588f674e47df88e85a1eb6258c6c8f494bb7996b
Heinz Knutzen <heinz.knutzen@dataport.de>
2021-05-25 15:17:36 +0200

    Merged duplicate code

--- go/pkg/filetree/walk.go
+++ go/pkg/filetree/walk.go
@@ -16,8 +16,9 @@ type Context struct {
 type parser func(*Context) error
 
 // Read input from file and process it by function which is given as argument.
-func processFile(input *Context, fn parser) error {
-	content, err := os.ReadFile(input.Path)
+func processFile(fname string, v6 bool, fn parser) error {
+	input := &Context{Path: fname, IPV6: v6}
+	content, err := os.ReadFile(fname)
 	if err != nil {
 		return err
 	}
@@ -27,66 +28,47 @@ func processFile(input *Context, fn parser) error {
 
 func Walk(fname string, fn parser) error {
 	v6 := conf.Conf.IPV6
-
-	// Handle toplevel file.
-	if !fileop.IsDir(fname) {
-		input := &Context{Path: fname, IPV6: v6}
-		return processFile(input, fn)
-	}
-
 	ipvDir := "ipv6"
-	if conf.Conf.IPV6 {
+	if v6 {
 		ipvDir = "ipv4"
 	}
 	ignore := conf.Conf.IgnoreFiles
 
-	// Handle toplevel Directory
-	files, err := os.ReadDir(fname)
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		base := file.Name()
+	var walk func(string, bool, bool) error
+	walk = func(fname string, v6, toplevel bool) error {
+		base := path.Base(fname)
 
-		// Skip special file/directory.
-		if base == "config" || base == "raw" {
-			continue
-		}
-		name := filepath.Join(fname, base)
-		var walk func(string, bool) error
-		walk = func(fname string, v6 bool) error {
-			base := path.Base(fname)
-
-			// Skip hidden and ignored file.
-			if base[0] == '.' || ignore.MatchString(base) {
-				return nil
-			}
-
-			// Handle ipv6 / ipv4 subdirectory or file.
-			if base == ipvDir {
-				v6 = base == "ipv6"
-			}
-
-			if !fileop.IsDir(fname) {
-				input := &Context{Path: fname, IPV6: v6}
-				return processFile(input, fn)
-			}
-			files, err := os.ReadDir(fname)
-			if err != nil {
-				panic(err)
-			}
-			for _, file := range files {
-				base := file.Name()
-				name := filepath.Join(fname, base)
-				if err := walk(name, v6); err != nil {
-					return err
-				}
-			}
+		// Skip hidden and ignored file.
+		if base[0] == '.' || ignore.MatchString(base) {
 			return nil
 		}
-		if err := walk(name, v6); err != nil {
-			return err
+
+		// Handle ipv6 / ipv4 subdirectory or file.
+		if base == ipvDir {
+			v6 = base == "ipv6"
 		}
+
+		if !fileop.IsDir(fname) {
+			return processFile(fname, v6, fn)
+		}
+		files, err := os.ReadDir(fname)
+		if err != nil {
+			panic(err)
+		}
+		for _, file := range files {
+			base := file.Name()
+
+			// Skip special file/directory.
+			if toplevel && (base == "config" || base == "raw") {
+				continue
+			}
+			name := filepath.Join(fname, base)
+			if err := walk(name, v6, false); err != nil {
+				return err
+			}
+		}
+		return nil
 	}
-	return nil
+	toplevel := true
+	return walk(fname, v6, toplevel)
 }
