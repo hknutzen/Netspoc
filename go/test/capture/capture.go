0c910e14a1cb986285d14e5d61c639072e6cb368
Heinz Knutzen <heinz.knutzen@dataport.de>
2022-05-27 17:03:04 +0200

    Added again: func Capture

--- go/test/capture/capture.go
+++ go/test/capture/capture.go
@@ -1,9 +1,36 @@
 package capture
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"os"
 )
+
+func Capture(std **os.File, f func()) string {
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	old := *std
+	*std = w
+	defer func() {
+		*std = old
+	}()
+
+	out := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		out <- buf.String()
+	}()
+
+	f()
+
+	w.Close()
+	return <-out
+}
 
 func CatchPanic(stderr io.Writer, f func() int) (status int) {
 	defer func() {
