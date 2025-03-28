f4cda2e22ad1538d81bc2c0f8077817bac412fa9
Dominik Kunkel <dominik.kunkel@dataport.de>
2025-03-28 12:42:31 +0100

    Add Checkpoint test to netspoc_test

--- go/test/netspoc_test.go
+++ go/test/netspoc_test.go
@@ -51,7 +51,8 @@ type test struct {
 var tests = []test{
 	{".", outDirT, pass1.SpocMain, netspocCheck},
 	{"ipv6", outDirT, pass1.SpocMain, netspocCheck},
-	{"export-netspoc", outDirT, pass1.ExportMain, exportCheck},
+	{"checkpoint", outDirT, pass1.SpocMain, jsonFilesCheck},
+	{"export-netspoc", outDirT, pass1.ExportMain, jsonFilesCheck},
 	{"format-netspoc", chgInputT, format.Main, formatCheck},
 	{"add-to-netspoc", chgInputT, addto.Main, chgInputCheck},
 	{"expand-group", chgInputT, expand.Main, chgInputCheck},
@@ -396,7 +397,7 @@ func getBlocks(t *testing.T, data string, blocks []string) string {
 	return out
 }
 
-func exportCheck(t *testing.T, spec, dir string) {
+func jsonFilesCheck(t *testing.T, spec, dir string) {
 	// Blocks of expected output are split by single lines of dashes,
 	// followed by file name.
 	re := regexp.MustCompile(`(?ms)^-+[ ]*\S+[ ]*\n`)
