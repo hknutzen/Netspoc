04151ae4b3de4b915e265ee7f21dbb1fb31a14b7
Dominik Kunkel <dominik.kunkel@dataport.de>
2025-07-08 15:22:48 +0200

    Zwischenstand

--- go/pkg/pass1/setup-objects.go
+++ go/pkg/pass1/setup-objects.go
@@ -2801,11 +2801,12 @@ var routerInfo = map[string]*model{
 		noACLself:              true,
 		noSharedHardware:       true,
 	},
-	"Checkpoint": {
-		routing:                "Checkpoint",
+	"CHECKPOINT": {
+		routing:                "",
 		canVRF:                 true,
 		needVRF:                true,
 		needManagementInstance: true,
+		statelessICMP:          true,
 	},
 	"Linux": {
 		routing:     "iproute",
