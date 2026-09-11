932c3f7a0372f5716288d43d1d48b1599170507e
Heinz Knutzen <heinz.knutzen@dataport.de>
2026-09-11 12:47:57 +0200

    Changed omitempty to omitzero for json/v2

--- go/pkg/jcode/types.go
+++ go/pkg/jcode/types.go
@@ -8,7 +8,7 @@ import (
 type RouterData struct {
 	Model         string     `json:"model"`
 	ACLs          []*ACLInfo `json:"acls"`
-	DoObjectgroup bool       `json:"do_objectgroup,omitempty"`
+	DoObjectgroup bool       `json:"do_objectgroup,omitzero"`
 }
 
 type ACLInfo struct {
@@ -18,25 +18,25 @@ type ACLInfo struct {
 	OptNetworks  []string `json:"opt_networks,omitempty"`
 	NoOptAddrs   []string `json:"no_opt_addrs,omitempty"`
 	NeedProtect  []string `json:"need_protect,omitempty"`
-	AddPermit    bool     `json:"add_permit,omitempty"`
-	AddDeny      bool     `json:"add_deny,omitempty"`
-	FilterAnySrc bool     `json:"filter_any_src,omitempty"`
+	AddPermit    bool     `json:"add_permit,omitzero"`
+	AddDeny      bool     `json:"add_deny,omitzero"`
+	FilterAnySrc bool     `json:"filter_any_src,omitzero"`
 	FilterOnly   []string `json:"filter_only,omitempty"`
-	IsStdACL     bool     `json:"is_std_acl,omitempty"`
-	IsCryptoACL  bool     `json:"is_crypto_acl,omitempty"`
-	Tier         string   `json:"tier,omitempty"`
-	VRF          string   `json:"vrf,omitempty"`
-	LogDeny      string   `json:"log_deny,omitempty"`
+	IsStdACL     bool     `json:"is_std_acl,omitzero"`
+	IsCryptoACL  bool     `json:"is_crypto_acl,omitzero"`
+	Tier         string   `json:"tier,omitzero"`
+	VRF          string   `json:"vrf,omitzero"`
+	LogDeny      string   `json:"log_deny,omitzero"`
 }
 
 type Rule struct {
-	Deny         bool     `json:"deny,omitempty"`
+	Deny         bool     `json:"deny,omitzero"`
 	Src          []string `json:"src"`
 	Dst          []string `json:"dst"`
 	Prt          []string `json:"prt"`
-	SrcRange     string   `json:"src_range,omitempty"`
-	Log          string   `json:"log,omitempty"`
-	OptSecondary bool     `json:"opt_secondary,omitempty"`
+	SrcRange     string   `json:"src_range,omitzero"`
+	Log          string   `json:"log,omitzero"`
+	OptSecondary bool     `json:"opt_secondary,omitzero"`
 }
 
 // GenPortName is used to create name of protocol with ports printed
